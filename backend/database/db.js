8718b8fff7949b6971ef614c5da775aa6266dc88
Ritik <ritik240gwalior@gmail.com>
2024-06-17 15:19:13 +0530

    d13

--- backend/database/db.js
+++ backend/database/db.js
@@ -1,25 +1,20 @@
-import {Pool} from 'pg
 import pg from "pg"
 import dotenv from 'dotenv'
 dotenv.config();
 const port = process.env.DBPORT
-// const db = new pg.Client(
-//     {
-//        user:process.env.PGUSERNAME,
-//        host:process.env.HOSTNAME,
-//        database:process.env.DBNAME,
-//        password:process.env.DBPASSWORD,
-//        port:process.env.DBPORT,
-//        ssl: {
-//         rejectUnauthorized:false,
-//        },
-//     });
+const db = new pg.Client(
+    {
+       user:process.env.PGUSERNAME,
+       host:process.env.HOSTNAME,
+       database:process.env.DBNAME,
+       password:process.env.DBPASSWORD,
+       port:process.env.DBPORT,
+       ssl: {
+        rejectUnauthorized:false,
+       },
+    });
 
 
-const db = new Pool({
-  connectionString: "postgres://default:Cd81yQzSeqam@ep-misty-leaf-a4cet9sa-pooler.us-east-1.aws.neon.tech:5432/verceldb?sslmode=require?sslmode=require",
-})
-    
  
 async function connection()
 {
